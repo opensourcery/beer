f4fb896f7015b677713e2b666b7ae2f8ae9b484f
Jonathan Hedstrom <jhedstrom@gmail.com>
2014-01-02 18:10:48 -0800

    Some failing tests.

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -3,12 +3,12 @@
 /* jasmine specs for services go here */
 
 describe('service', function() {
-  beforeEach(module('myApp.services'));
+  beforeEach(module('osBeer.services'));
 
 
-  describe('version', function() {
-    it('should return current version', inject(function(version) {
-      expect(version).toEqual('0.1');
+  describe('Kegs', function() {
+    it('should return 2 kegs', inject(function(Kegs) {
+      expect(Kegs.query().length).toEqual(2);
     }));
   });
 });
