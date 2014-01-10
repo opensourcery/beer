84c914a3c7436ad5ed5d71cc561d99f2c746b100
Jonathan Hedstrom <jhedstrom@gmail.com>
2014-01-09 17:56:08 -0800

    Adding an environment controller.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,4 +7,9 @@ angular.module('osBeer.controllers', []).
     function($scope, Kegs) {
       $scope.kegs = Kegs.query();
     }
+  ]).
+  controller('Kegerator', ['$scope', 'Kegerator',
+    function($scope, Kegerator) {
+      $scope.environment = Kegerator.query();
+    }
   ]);
--- app/js/services.js
+++ app/js/services.js
@@ -12,4 +12,11 @@ angular.module('osBeer.services', ['ngResource']).
         query: {method: 'GET', params:{kegId:'kegs'}, isArray:true}
       });
     }
+  ]).
+  factory('Kegerator', ['$resource',
+    function($resource){
+      return $resource('kegerator.json', {}, {
+        query: {method: 'GET'}
+      });
+    }
   ]);
