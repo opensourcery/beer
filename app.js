e871dd525071ee4a90ea0234d43204e261a51d1f
Jonathan Hedstrom <jhedstrom@gmail.com>
2014-02-27 15:06:35 -0800

    Initial API placeholder.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 
 var express = require('express'),
   routes = require('./routes'),
+  api = require('./routes/api'),
   http = require('http'),
   path = require('path');
 
@@ -20,7 +21,6 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(app.router);
 
 // development only
 if (app.get('env') === 'development') {
@@ -32,16 +32,54 @@ if (app.get('env') === 'production') {
   // TODO
 };
 
+/**
+ * Configure API access.
+ */
+var passport = require('passport'),
+    BearerStrategy = require('passport-http-bearer').Strategy,
+    apiSecret = require('./config/api.js');
+
+function findByToken(token, fn) {
+  for (var i = 0, len = apiSecret.users.length; i < len; i++) {
+    var user = apiSecret.users[i];
+    if (user.token === token) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
+
+passport.use(new BearerStrategy({},
+  function(token, done) {
+    findByToken(token, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      return done(null, user, { scope: 'read' });
+    });
+  }
+));
+
+app.configure(function () {
+  app.use(passport.initialize());
+  app.use(app.router);
+});
 
 /**
  * Routes
  */
 
-// serve index and view partials
+// Serve index.
 app.get('/', routes.index);
-//app.get('/partials/:name', routes.partials);
 
-// redirect all others to the index (HTML5 history)
+// API requests.
+app.post('/api',
+  passport.authenticate('bearer', {session: false})
+, api.environment);
+
+// Provide something for GET requests.
+app.get('/api', api.landingPage);
+
+// Redirect all others to the index (HTML5 history).
 app.get('*', routes.index);
 
 // Socket.io Communication
@@ -50,7 +88,6 @@ io.sockets.on('connection', require('./routes/socket'));
 /**
  * Start Server
  */
-
 server.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
---
+++ routes/api.js
@@ -0,0 +1,11 @@
+/**
+ * A POST API to update keg levels and environment variables.
+ */
+
+exports.environment = function (req, res) {
+  res.json({response: 'OK'});
+}
+
+exports.landingPage = function (req, res) {
+  res.json({response: 'API is active'});
+}
