48124151f66cabb26baa48221422020543752462
Jonathan Hedstrom <jhedstrom@gmail.com>
2014-02-27 15:52:53 -0800

    Remove time push.

--- routes/socket.js
+++ routes/socket.js
@@ -81,10 +81,4 @@ module.exports = function (socket) {
     }
     socket.emit('send:environment', kegerator);
   });
-
-  setInterval(function () {
-    socket.emit('send:time', {
-      time: (new Date()).toString()
-    });
-  }, 1000);
 };
