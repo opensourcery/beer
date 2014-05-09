5d501295372ad9063ba271e227fd5ca77e28dafe
Jonathan Hedstrom <jhedstrom@gmail.com>
2014-05-08 17:15:48 -0700

    Process deltas instead of percentages.

--- routes/api.js
+++ routes/api.js
@@ -9,18 +9,45 @@ var redis = require('redis');
 var cache = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
 
 exports.environment = function (req, res) {
-  var kegerator = {
-    temperature: req.body.temperature,
-    kegs: JSON.parse(req.body.kegs)
-  }
 
-  // Update the kegerator environment.
-  req.app.locals.io.sockets.emit('send:environment', kegerator);
-  res.json({response: 'OK'});
-  req.app.locals.io.log.info('Updating environment from API request by user "' + req.user.username + '".');
+  // Load stored environment.
+  cache.get('kegerator.environment', function (err, result) {
+    var kegerator = {};
+    if (err || !result) {
+      // This should not happen, but just in case, assume full kegs.
+      kegerator.kegs = [1, 1];
+    }
+    else {
+      kegerator = JSON.parse(result);
+    }
 
-  // Update cache.
-  cache.setex('kegerator.environment', 3600 * 24 * 7, JSON.stringify(kegerator));
+    // Reset.
+    if (typeof req.body.reset != 'undefined') {
+      var resets = JSON.parse(req.body.reset);
+      for (tapNumber in resets) {
+        if (resets[tapNumber]) {
+          kegerator.kegs[tapNumber] = 1;
+          req.app.locals.io.log.info('Reset keg ' + (parseInt(tapNumber) + parseInt(1)) + ' to full');
+        }
+      }
+    }
+
+    // Updates contain deltas rather than actual keg percentages.
+    var deltas = JSON.parse(req.body.kegs);
+    for (var tapNumber in kegerator.kegs) {
+      kegerator.kegs[tapNumber] = Math.max(kegerator.kegs[tapNumber] - deltas[tapNumber], 0);
+    }
+
+    kegerator.temperature = req.body.temperature;
+
+    // Update the kegerator environment.
+    req.app.locals.io.sockets.emit('send:environment', kegerator);
+    res.json({response: 'OK'});
+    req.app.locals.io.log.info('Updating environment from API request by user "' + req.user.username + '".');
+
+    // Update cache.
+    cache.set('kegerator.environment', JSON.stringify(kegerator));
+  });
 }
 
 exports.landingPage = function (req, res) {
